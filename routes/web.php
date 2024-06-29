e24aa386201d4ee5b9c2552b7aefe5aa1fa5d2c8
Igor Machado <igor.gabriel.machado@gmail.com>
2024-06-29 14:06:55 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/auth/github/redirect', function () {
+    return Socialite::driver('github')->stateless()->redirect();
+});
+ 
+Route::get('/auth/github/callback', function () {
+    $githubUser = Socialite::driver('github')->stateless()->user();
+ 
+    $user = User::updateOrCreate([
+        'github_id' => $githubUser->id,
+    ], [
+        'name' => $githubUser->name,
+        'email' => $githubUser->email,
+        'github_token' => $githubUser->token,
+        'github_refresh_token' => $githubUser->refreshToken,
+    ]);
+ 
+    Auth::login($user);
+ 
+    return view('/dashboard');
+});
+
+require __DIR__.'/auth.php';
