00f1c1a0615dde45af9a8c1a21f4785e2d201797
Igor Machado <igor.gabriel.machado@gmail.com>
2024-06-29 14:14:04 -0300

    second commit

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'github' => [
     'client_id' => env('GITHUB_CLIENT_ID'),
     'client_secret' => env('GITHUB_CLIENT_SECRET'),
-    'redirect' => 'http://localhost:8000/auth/github/callback',
+    'redirect' => 'https://laravelgitrepos-production.up.railway.app/auth/github/callback',
 ],
 
 ];
